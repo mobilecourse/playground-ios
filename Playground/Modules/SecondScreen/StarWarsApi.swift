6127fce4714bc61f6c8b1297390d17a213905869
Krzysztof Kubicki <krzysztof.kubicki@nestbank.pl>
2019-07-01 15:38:53 +0200

    Attempting to perform some JSON decoding into swift Struct

--- Playground/Modules/SecondScreen/StarWarsApi.swift
+++ Playground/Modules/SecondScreen/StarWarsApi.swift
@@ -18,13 +18,34 @@ class StarWarsApi {
             print("Error: \(String(describing: response.error))")
             
             if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)")
+//                print("Data: \(utf8Text)")
+                
+                
+                let starWarsPeron = try? JSONDecoder().decode(StarWarsPeron.self, from: utf8Text)
+                
+                print(starWarsPeron.name)
+                print(starWarsPeron.homeworld)
+
             }
         }
     }
 }
 
-//struct StarWarsPerson {
-//    var name: String,
-//    var someOtherValue: String
-//}
+// MARK: - StarWarsPeron
+struct StarWarsPeron: Codable {
+    let name, height, mass, hairColor: String
+    let skinColor, eyeColor, birthYear, gender: String
+    let homeworld: String
+    let films, species, vehicles, starships: [String]
+    let created, edited: String
+    let url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, height, mass
+        case hairColor = "hair_color"
+        case skinColor = "skin_color"
+        case eyeColor = "eye_color"
+        case birthYear = "birth_year"
+        case gender, homeworld, films, species, vehicles, starships, created, edited, url
+    }
+}
